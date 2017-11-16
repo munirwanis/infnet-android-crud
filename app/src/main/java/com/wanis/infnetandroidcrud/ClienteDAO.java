384a5c585c5f5c0bd0022ec9c802c21c01853cbf
Munir Xavier Wanis <munir.wanis@SJ208F08.local>
2017-11-16 12:05:43 -0200

    🚧  First Commit

---
+++ app/src/main/java/com/wanis/infnetandroidcrud/ClienteDAO.java
@@ -0,0 +1,45 @@
+package com.wanis.infnetandroidcrud;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by munir.wanis on 16/11/17.
+ */
+
+public class ClienteDAO {
+    private final String TABLE_CLIENTES = "Clientes";
+    private DbGateway gw;
+
+    public ClienteDAO(Context ctx){
+        gw = DbGateway.getInstance(ctx);
+    }
+
+    public boolean salvar(String nome, String sexo, String uf, boolean vip){
+        ContentValues cv = new ContentValues();
+        cv.put("Nome", nome);
+        cv.put("Sexo", sexo);
+        cv.put("UF", uf);
+        cv.put("Vip", vip ? 1 : 0);
+        return gw.getDatabase().insert(TABLE_CLIENTES, null, cv) > 0;
+    }
+
+    public List<Cliente> retornarTodos(){
+        List<Cliente> clientes = new ArrayList<>();
+        Cursor cursor = gw.getDatabase().rawQuery("SELECT * FROM Clientes", null);
+        while(cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex("ID"));
+            String nome = cursor.getString(cursor.getColumnIndex("Nome"));
+            String sexo = cursor.getString(cursor.getColumnIndex("Sexo"));
+            String uf = cursor.getString(cursor.getColumnIndex("UF"));
+            boolean vip = cursor.getInt(cursor.getColumnIndex("Vip")) > 0;
+            clientes.add(new Cliente(id, nome, sexo, uf, vip));
+        }
+        cursor.close();
+        return clientes;
+    }
+}
---
+++ app/src/main/java/com/wanis/infnetandroidcrud/MainActivity.java
@@ -0,0 +1,110 @@
+package com.wanis.infnetandroidcrud;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.includeMain).setVisibility(View.INVISIBLE);
+                findViewById(R.id.includecadastro).setVisibility(View.VISIBLE);
+                findViewById(R.id.fab).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        Button btnCancelar = (Button)findViewById(R.id.btnCancelar);
+        btnCancelar.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.includeMain).setVisibility(View.VISIBLE);
+                findViewById(R.id.includecadastro).setVisibility(View.INVISIBLE);
+                findViewById(R.id.fab).setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button btnSalvar = (Button)findViewById(R.id.btnSalvar);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                salvarCliente(view);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void salvarCliente(View view) {
+        //carregando os campos
+        EditText txtNome = (EditText)findViewById(R.id.txtNome);
+        Spinner spnEstado = (Spinner)findViewById(R.id.spnEstado);
+        RadioGroup rgSexo = (RadioGroup)findViewById(R.id.rgSexo);
+        CheckBox chkVip = (CheckBox)findViewById(R.id.chkVip);
+
+        //pegando os valores
+        String nome = txtNome.getText().toString();
+        String uf = spnEstado.getSelectedItem().toString();
+        boolean vip = chkVip.isChecked();
+        String sexo = rgSexo.getCheckedRadioButtonId() == R.id.rbMasculino ? "M" : "F";
+
+        //salvando os dados
+        ClienteDAO dao = new ClienteDAO(getBaseContext());
+        boolean sucesso = dao.salvar(nome, sexo, uf, vip);
+        if(sucesso) {
+            //limpa os campos
+            txtNome.setText("");
+            rgSexo.setSelected(false);
+            spnEstado.setSelection(0);
+            chkVip.setChecked(false);
+
+            Snackbar.make(view, "Salvou!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            findViewById(R.id.includeMain).setVisibility(View.VISIBLE);
+            findViewById(R.id.includecadastro).setVisibility(View.INVISIBLE);
+            findViewById(R.id.fab).setVisibility(View.VISIBLE);
+        }else{
+            Snackbar.make(view, "Erro ao salvar, consulte os logs!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    }
+}
